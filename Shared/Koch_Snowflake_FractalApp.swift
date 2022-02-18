0bb5001973f2f61c7e70865374c574220afbd5bb
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-18 15:48:43 -0600

    Initial Commit

---
+++ Shared/Koch_Snowflake_FractalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koch_Snowflake_FractalApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Koch_Snowflake_FractalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
